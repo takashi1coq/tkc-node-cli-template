5911d3355afd5aa90ffc406c8c3979394ac6815c
takashi1coq <maedaw1nb@gmail.com>
2020-02-20 15:53:45 +0900

    bug fix

--- config/webpack.common.js
+++ config/webpack.common.js
@@ -1,7 +1,8 @@
 const webpack = require('webpack');
+const path = require('path');
 
 module.exports = {
-  entry: './src/index.ts',
+  entry: path.resolve('src/index.ts'),
   target: 'node',
   plugins: [
       new webpack.BannerPlugin({ banner: "#!/usr/bin/env node", raw: true }),
